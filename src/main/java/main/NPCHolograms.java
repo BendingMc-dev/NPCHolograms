7c85e32a768981f345fed78d200d6cf3e8fe995c
RyanDusty <RyanDusty@users.noreply.github.com>
2025-02-02 22:18:15 +1100

    I cant remember what I did but im onto something pls dont close google tabs i can get this tomorrow

--- src/main/java/main/NPCHolograms.java
+++ src/main/java/main/NPCHolograms.java
@@ -1,27 +1,42 @@
 package main;
 
 import de.oliver.fancyholograms.api.FancyHologramsPlugin;
+import net.citizensnpcs.Citizens;
+import net.citizensnpcs.api.CitizensAPI;
+import org.betonquest.betonquest.BetonQuest;
 import org.betonquest.betonquest.api.logger.BetonQuestLoggerFactory;
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+
+
 
 public final class NPCHolograms extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
+        // Runs other classes
         getServer().getPluginManager().registerEvents(new NPCInteract(), this);
-
-
+        // Variables to check if APIS are running
         FancyHologramsPlugin hologramsLoaded = FancyHologramsPlugin.get();
-        BetonQuestLoggerFactory loggerFactory = getServer().getServicesManager().load(BetonQuestLoggerFactory.class);
+
+        BetonQuest instance = BetonQuest.getInstance();
+
+        Plugin citizensInstance = CitizensAPI.getPlugin();
+        // Logs to make sure API has loaded.
+        if(CitizensAPI.getPlugin().isEnabled())
+            getLogger().info("Citizens API has loaded! " + citizensInstance);
+        else
+            getLogger().info("Citizens API hasn't loaded! " + citizensInstance);
         if(hologramsLoaded == null)
-            getLogger().info("Fancy Holograms API has not loaded." + hologramsLoaded);
+            getLogger().info("Fancy Holograms API has not loaded: " + hologramsLoaded);
         else
-            getLogger().info("Fancy Holograms API has loaded:" + hologramsLoaded);
-        if (loggerFactory == null)
-            getLogger().info("BetonQuest has not loaded!" + loggerFactory);
+            getLogger().info("Fancy Holograms API has loaded: " + hologramsLoaded);
+        if (instance == null)
+            getLogger().info("BetonQuest has not loaded: " + instance);
         else
-            getLogger().info("BetonQuest api has loaded!" + loggerFactory); {
+            getLogger().info("BetonQuest api has loaded: "  + instance); {
         }
     }
 
--- src/main/java/main/NPCInteract.java
+++ src/main/java/main/NPCInteract.java
@@ -2,9 +2,15 @@ package main;
 
 
 
+import net.citizensnpcs.api.CitizensAPI;
 import net.citizensnpcs.api.event.NPCRightClickEvent;
 import net.citizensnpcs.api.npc.NPC;
+import org.betonquest.betonquest.BetonQuest;
+import org.betonquest.betonquest.compatibility.citizens.CitizensConversation;
+import org.betonquest.betonquest.conversation.Conversation;
+import org.betonquest.betonquest.quest.event.conversation.ConversationEvent;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,9 +21,16 @@ public class NPCInteract implements Listener {
     public void NPCInteractStart(NPCRightClickEvent event) {
         NPC npc = event.getNPC();
         Player player = event.getClicker();
+        String npcName = npc.getFullName();
+        Location npcLocation = npc.getStoredLocation();
         if(npc != null)
-            player.sendRawMessage("I just interacted with an NPC!");
+            player.sendRawMessage("I just interacted with " + npcName + "! Jack is located at " + npcLocation + "!");
         Bukkit.getLogger().info("NPC:" + npc + "Player:" + player);
         }
-    }
+        public void NPCDialogue(CitizensConversation citizensConversation) {
+        //    Conversation conversation = Conversation.getConversation();
+            }
 
+        }
+
+// Interact -> Identify if it's a BQ npc!
